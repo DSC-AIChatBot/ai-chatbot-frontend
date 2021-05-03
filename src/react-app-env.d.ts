b448bd896dd6ec0411ee178445589d463c23dab8
KIMBEOBWOO <qjqdn1568@naver.com>
2021-05-03 17:00:07 +0900

    error fix

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -11,7 +11,6 @@
 //     export { useForm };
 // }
 
-<<<<<<< HEAD
 // declare module "react-router-dom" {
 //     export { BrowserRouter, Switch, Route, Link };
 // }
@@ -22,15 +21,3 @@
 // declare module '@material-ui/core' {
 //     export { Button, useStyles };
 // }
-=======
-declare module "react-router-dom" {
-    export { BrowserRouter, Switch, Route, Link };
-}
-
-declare module 'react' {
-    export { useState, JSXElementConstructor };
-}
-declare module '@material-ui/core' {
-    export { Button, useStyles, StylesProvider };
-}
->>>>>>> 0dedc54dd9a412494ec7726ee4901a776e5f481a
