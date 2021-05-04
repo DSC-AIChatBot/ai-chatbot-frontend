39e008937a42d2f9cba10c53a145cbed9d489f50
harrysci <59914405+harrysci@users.noreply.github.com>
2021-05-04 14:28:34 +0900

    달력 레이아웃 수정

--- src/pages/FeelCalendarPage/index.tsx
+++ src/pages/FeelCalendarPage/index.tsx
@@ -2,47 +2,45 @@ import React, { useState } from 'react';
 //import { Grid } from '@material-ui/core';
 import moment from 'moment';
 import { List, Grid } from '@material-ui/core';
-function RecordFeelSchedule () {
-  const [getMoment, setMoment] = useState<any>(moment().toString());
-  const today = getMoment; // today==moment()
-  const firstWeek = useState<any>(today.clone().startOf('month').week());
-  const lastWeek = useState<any>(today.clone().endOf('month').week() === 1
-    ? 53 : today.clone().endOf('month').week());
-
-  //const [week, setWeek]=useState<string>(firstWeek);
+function FeelSchedule () {
+  const [getMoment, setMoment] = useState<string>(moment().format('YYYY-MM'));
+  const [today, setToday] = useState<string>(getMoment); // today==moment()
+  const curr = moment(getMoment);
+  const [firstWeek, setFirstWeek] = useState<number>(curr.startOf('month').week());
+  const [lastWeek, setLastWeek] = useState<number>(curr.endOf('month').week() === 1
+    ? 53 : curr.endOf('month').week());
   const makeCalendar = () => {
-    const result = useState<string[]>([]);
-    let week = useState<string>(firstWeek);
-    for (week; week <= lastWeek; week++) {
-
-      <List>
-        {
-          result.map()
-        }
-      </List>;
-
-    }
+    const [result, setResult] = useState<JSX.Element>(<div></div>);
+    //const [week, setWeek] = useState<number>(firstWeek);
+    // const [day, setDay] =
+    //     useState<number>(curr.week(week).startOf('day').day());
+    //for (firstWeek; firstWeek <= lastWeek; setFirstWeek(firstWeek + 1)) {
+    setResult(
+      <div>
+        <text>firstWeek</text>
+      </div>,
+    );
+    //}
     return result;
   };
 
   return (
-    <Grid className="App">
+    <div className="App">
       <div className="control">
         <button onClick={() => {
-          setMoment(getMoment.clone().subtract(1, 'month')) //clone() 새로운 객체를 반환
+          setMoment(curr.subtract(1, 'month').format('YYYY-MM')) //clone() 새로운 객체를 반환
           ;
         }}>이전달</button>
-        <span>{today.format('YYYY 년 MM 월')}</span>
+        <span>{getMoment}</span>
         <button onClick={() => {
-          setMoment(getMoment.clone().add(1, 'month'))
+          setMoment(curr.add(1, 'month').format('YYYY-MM'))
           ;
         }}>다음달</button>
       </div>
-      <table style={{ display: 'flex', width: 50, height: 50, flexDirection: 'column' }}>
-        <tbody style={{ display: 'flex', flexDirection: 'column' }}>{makeCalendar()}</tbody>
-      </table>
-    </Grid>
+      <div>{makeCalendar}</div>
+
+    </div>
   );
 }
 
-export default RecordFeelSchedule;
+export default FeelSchedule;
