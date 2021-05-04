58f8dd9bd58bef79d7890642dd437f8a593917a6
harrysci <59914405+harrysci@users.noreply.github.com>
2021-05-04 15:32:40 +0900

    리렌더링 오류 수정

--- src/pages/FeelCalendarPage/index.tsx
+++ src/pages/FeelCalendarPage/index.tsx
@@ -1,29 +1,23 @@
-import React, { useState } from 'react';
+import * as React from 'react';
 //import { Grid } from '@material-ui/core';
 import moment from 'moment';
 import { List, Grid } from '@material-ui/core';
 function FeelSchedule () {
-  const [getMoment, setMoment] = useState<string>(moment().format('YYYY-MM'));
-  const [today, setToday] = useState<string>(getMoment); // today==moment()
+  const [getMoment, setMoment] = React.useState<string>(moment().format('YYYY-MM'));
+  const [today, setToday] = React.useState<string>(getMoment); // today==moment()
   const curr = moment(getMoment);
-  const [firstWeek, setFirstWeek] = useState<number>(curr.startOf('month').week());
-  const [lastWeek, setLastWeek] = useState<number>(curr.endOf('month').week() === 1
+  const [firstWeek, setFirstWeek] = React.useState<number>(curr.startOf('month').week());
+  const [lastWeek, setLastWeek] = React.useState<number>(curr.endOf('month').week() === 1
     ? 53 : curr.endOf('month').week());
-  const makeCalendar = () => {
-    const [result, setResult] = useState<JSX.Element>(<div></div>);
-    //const [week, setWeek] = useState<number>(firstWeek);
+  const makeCalendar = (week:number) => {
+    const [result, setResult] = React.useState<number>();
     // const [day, setDay] =
     //     useState<number>(curr.week(week).startOf('day').day());
-    //for (firstWeek; firstWeek <= lastWeek; setFirstWeek(firstWeek + 1)) {
-    setResult(
-      <div>
-        <text>firstWeek</text>
-      </div>,
-    );
-    //}
+    for (week; week <= lastWeek; week + 1) {
+      setResult(week);
+    }
     return result;
   };
-
   return (
     <div className="App">
       <div className="control">
@@ -37,10 +31,9 @@ function FeelSchedule () {
           ;
         }}>다음달</button>
       </div>
-      <div>{makeCalendar}</div>
+      <div className="calendar">{makeCalendar(firstWeek)}</div>
 
     </div>
   );
 }
-
 export default FeelSchedule;
