ff05381df2fa0242a0561a6f1e9ef1c0bc37c472
harrysci <59914405+harrysci@users.noreply.github.com>
2021-05-19 16:13:58 +0900

    달력 디자인 수정

--- src/pages/FeelCalendarPage/index.tsx
+++ src/pages/FeelCalendarPage/index.tsx
@@ -2,16 +2,16 @@ import React, { useState } from 'react';
 //import { Grid } from '@material-ui/core';
 import { useStyles } from './styles';
 import moment from 'moment';
-import { List, Grid } from '@material-ui/core';
-
+import { List, Grid, Button } from '@material-ui/core';
+import './styles.css';
 function FeelSchedule () {
   const classes = useStyles();
 
   const [getMoment, setMoment] = useState<string>(moment().format('YYYYMMDD'));
   const [today, setToday] = useState<string>(getMoment); // today==moment()
   let [result] = useState<JSX.Element[]>([]);
+  let [result2] = useState<JSX.Element[]>([]);
   const [test, setTest] = useState<JSX.Element[]>([]);
-
   const dayArr = ["일", "월", "화", "수", "목", "금", "토"];
 
   const calendar = () => {
@@ -21,7 +21,7 @@ function FeelSchedule () {
     for (let i = fWeek; i <= lWeek;i = i + 1) {
       console.log(i);
       result = result.concat(
-        <tr key={i}>
+        <Grid key={i} style={{ flexDirection: 'row' }}>
           {
             Array(7).fill(0).map((data, index) => {
 
@@ -31,78 +31,97 @@ function FeelSchedule () {
 
               if (moment().format('YYYYMMDD') === days.format('YYYYMMDD')) {
                 return (
-                  <td key={index} style={{ backgroundColor: 'rgb(246,120,80)',
-                    borderColor: 'black' }} >
-                    <span>{days.format('D')}</span>
+                  <td key={index}
+                    style={{ backgroundColor: 'rgb(246,231,110)', width: 250, height: 125 }} >
+
+                    <Grid
+                      style={{ backgroundColor: 'rgb(246,120,80)',
+                        justifyContent: 'center', display: 'flex' }}>{days.format('D')}</Grid>
+                    <Button className='day'>+</Button>
                   </td>
                 );
               } else if (days.format('MM') !== moment(getMoment).format('MM')) {
                 return (
-                  <td key={index} style={{ backgroundColor: 'gray', border: 5 }} >
-                    <span style={{ border: 5 }}>{days.format('D')}</span>
+                  <td key={index}
+                    style={{ backgroundColor: 'rgb(240,200,150)', width: 250, height: 125 }} >
+
+                    <Grid style={{ justifyContent: 'center',
+                      display: 'flex' }}>{days.format('D')}</Grid>
+                    <Button className='day' >+</Button>
                   </td>
                 );
-              } else if (days.day() === 0) {
+              } else if (days.day() === 0 || days.day() === 6) {
                 return (
-                  <td key={index} style={{ backgroundColor: 'rgb(210,80, 80)' }} >
-                    <span>{days.format('D')}</span>
-                  </td>
-                );
-              } else if (days.day() === 6) {
-                return (
-                  <td key={index} style={{ backgroundColor: 'rgb(80,100, 200)' }} >
-                    <span>{days.format('D')}</span>
+                  <td key={index}
+                    style={{ backgroundColor: 'rgb(250,210,90)',
+                      width: 250, height: 125 }} >
+
+                    <Grid style={{ justifyContent: 'center',
+                      display: 'flex' }}>{days.format('D')}</Grid>
+                    <Button className='day' >+</Button>
                   </td>
                 );
               } else {
                 return (
-                  <td key={index}style={{ backgroundColor: 'rgb(246,231,110)', border: 5 }} >
-                    <span>{days.format('D')}</span>
+                  <td key={index}
+                    className=''
+                    style={{ backgroundColor: 'rgb(246,231,110)', width: 250, height: 125 }} >
+
+                    <Grid style={{ justifyContent: 'center',
+                      display: 'flex' }}>{days.format('D')}</Grid>
+                    <Button className='day' >+</Button>
                   </td>
                 );
               }
             })
           }
-        </tr>,
+        </Grid>,
       );
       console.log(result);
     }
-    setTest(result);
-    return test;
+    return result;
+  };
+  const dayList = () => {
+    for (let i = 0 ; i < 7;i = i + 1) {
+      result2 = result2.concat(
+        <Grid style={{ marginLeft: 78, marginRight: 78 }}>
+          {dayArr[i]}
+        </Grid>,
+      );
+    }
+    return result2;
   };
   console.log(result.length);
   return (
     <div className="App" style={{ display: 'flex',
-      flexDirection: 'column', alignSelf: 'center' }}>
+      flexDirection: 'column', alignSelf: 'center', justifyContent: 'center' }}>
       <div className="control" style={{ display: 'flex',
         flexDirection: 'row', justifyContent: 'center' }}>
         <button onClick={() => {
           setMoment(moment(getMoment).subtract(1, 'month').format('YYYY-MM-DD'))
           ;
           console.log(getMoment);
-        }}>이전달</button>
-        <span style={{ fontSize: 30 }}>{moment(getMoment).format('YYYY 년 MM 월')}</span>
+        }}className='triangle test_3'>이전</button>
+        <span style={{ fontSize: 48 }}>{moment(getMoment).format('YYYY 년 MM 월')}</span>
         <button onClick={() => {
           setMoment(moment(getMoment).add(1, 'month').format('YYYY-MM-DD'))
           ;
           console.log(getMoment);
-        }}>다음달</button>
+        }}className='triangle test_4'>다음달</button>
 
       </div>
-      <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center',
+      <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center',
 
       }}>
-        <button style={{ width: 80, height: 40 }}onClick={() => {
-          calendar()
-          ;
-        }}>달력 생성</button></div>
+        <Grid style={{ marginTop: 20 }}>
+          <List style={{ justifyContent: 'center', display: 'flex', flexDirection: 'row' }}>
+            {dayList()}
+          </List></Grid>
 
-      <List style={{ paddingLeft: 500 }}>
-        {dayArr}
-      </List>
-      <table style={{ height: 700, width: 1100 }} >
-        <tbody className={classes.calendar}>{test}</tbody>
-      </table>
+        {/* <table style={{ height: 700, width: 1100 }} > */}
+        <Grid className={classes.calendar}>{calendar()}</Grid>
+        {/* </table> */}
+      </div>
     </div>
   );
 }
--- src/pages/FeelCalendarPage/styles.tsx
+++ src/pages/FeelCalendarPage/styles.tsx
@@ -3,8 +3,19 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export const useStyles = makeStyles(() => ({
   calendar: {
-    //display: 'flex',
+    display: 'flex',
     flexDirection: 'column',
     alignTtems: 'center',
+    height: 700,
+    width: 1200,
+    //padding: 100,
+  },
+  plusButton: {
+    width: 5,
+    height: 10,
+  },
+  todayButton: {
+    borderRadius: 10,
+    backgroundColor: 'red',
   },
 }));
